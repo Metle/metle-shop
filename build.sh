f37dde5f3c3cb388b7e696d962cddb2c6baacbf5
D Biervliet <d.biervliet@healthesolutions.nl>
2026-04-06 12:39:21 -0300

    env debug 7

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # build.sh
-pip install -r requirements.txt
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
