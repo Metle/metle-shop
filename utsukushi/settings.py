2a5b6921f846e9cfb7f6fe2aa07b66fe2f68afd9
D Biervliet <d.biervliet@healthesolutions.nl>
2026-04-06 11:52:40 -0300

    env files

--- utsukushi/settings.py
+++ utsukushi/settings.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from decouple import config
+
+from decouple import Csv, config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-me'
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me')
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
