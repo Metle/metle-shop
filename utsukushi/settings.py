3f3fc3b96e7665db654c5d347e32529385a70940
D Biervliet <d.biervliet@healthesolutions.nl>
2026-04-07 10:28:50 -0300

    vercel config 5

--- utsukushi/settings.py
+++ utsukushi/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 LOCALE_PATHS = [BASE_DIR / 'locale']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
